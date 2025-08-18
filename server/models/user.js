b29d4ea382e6621c53b5974bfca650add6180e6f
Farzana-Saba <farzana.faiza.saba@g.bracu.ac.bd>
2025-08-18 14:13:35 +0600

    Add files via upload

--- server/models/user.js
+++ server/models/user.js
@@ -1,35 +1,15 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  gender: {
-    type: String,
-    required: false,
-  },
-  role: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-}, {
-  timestamps: true
-});
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    gender: String,
+    role: String,
+    password: String,
+    preferences: {
+        preferredBuses: [String],
+        avoidRoutes: [String]
+    }
+}, { timestamps: true });
 
-const User = mongoose.model('User', userSchema);
-
-// Add some debugging to see what collection is being used
-console.log('User model created with collection name:', User.collection.name);
-console.log('User model database:', User.db.name);
-
-module.exports = User;
+module.exports = mongoose.model('User', UserSchema);
